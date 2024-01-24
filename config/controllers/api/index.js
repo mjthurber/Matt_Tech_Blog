d245ff9ea668f33d29996021e184ce356bbe80b6
mjthurber <mjthurber@gmail.com>
2024-01-23 19:56:41 -0700

    removed unnecessary files from config

--- config/controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
-
-router.use('/users', userRoutes);
-router.use('/blogs', blogRoutes);
-
-module.exports = router;
